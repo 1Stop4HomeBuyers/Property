e4f56a39fbda63c7b44177d719a573fed347d89e
1stop4homebuyer <office@1stop4homebuyer.co.uk>
2019-05-17 10:45:18 +0100

    initial file

---
+++ HelloMadhuri.java
@@ -0,0 +1,6 @@
+public class HelloMadhuri {
+   public static void main(String[] args) {
+      // Prints "Hello, Madhuri" in the terminal window.
+      System.out.println("Hello, Madhuri");
+   }
+}
